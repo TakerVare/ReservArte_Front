1bf23006946dbfc8d5f7cbf876e52124c174a72d
Gasvm <28313@svalero.com>
2026-05-23 18:03:58 +0200

    Añadidos botones de filtro por tipo y estados

--- ReservArte/src/views/SupplierMasterView.vue
+++ ReservArte/src/views/SupplierMasterView.vue
@@ -38,6 +38,58 @@ export default { name: 'SupplierMasterView' }
   <div class="supplier-master-view">
     <CPageTittle :text="t('supplier.title')" :size="size" />
 
+    <div class="supplier-master-view__container">
+      <div class="supplier-master-view__nav">
+        <CNavAreaPrimaryButton
+          :text="t('common.back')"
+          :size="size"
+          icon-position="before"
+          @click="onBack"
+        >
+          <template #icon>
+            <img src="../assets/arrowLeft.svg" alt="" />
+          </template>
+        </CNavAreaPrimaryButton>
+      </div>
+
+      <!-- Botones de filtro por tipo -->
+      <div class="supplier-master-view__filters">
+        <button
+          v-for="type in supplierStore.supplierTypes"
+          :key="type"
+          class="supplier-master-view__filter-btn"
+          :class="{ 'supplier-master-view__filter-btn--active': supplierStore.selectedType === type }"
+          @click="selectType(type)"
+        >
+          {{ type }}
+        </button>
+      </div>
+
+      <!-- Error -->
+      <p v-if="supplierStore.error" class="supplier-master-view__error">
+        {{ supplierStore.error }}
+      </p>
+
+      <!-- Loading -->
+      <p v-else-if="supplierStore.loading" class="supplier-master-view__loading">
+        {{ t('common.loading') }}
+      </p>
+
+      <!-- Lista -->
+      <div v-else class="supplier-master-view__list">
+        <CSupplierRow
+          v-for="supplier in supplierStore.filteredSuppliers"
+          :key="supplier.id"
+          :name="supplier.name"
+          :phone="supplier.phone"
+          :address="supplier.address"
+          :type="supplier.type"
+        />
+        <p v-if="supplierStore.filteredSuppliers.length === 0" class="supplier-master-view__empty">
+          {{ t('supplier.empty') }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
